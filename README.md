415e5d8a74d7c7dcc59bbb0acf11af6dea0e31de
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2022-07-23 00:10:55 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ConvertViewToPNGDataModule
+
+A description of this package.
---
+++ Sources/ConvertViewToPNGDataModule/ConvertViewToPNGDataModule.swift
@@ -0,0 +1,6 @@
+public struct ConvertViewToPNGDataModule {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
